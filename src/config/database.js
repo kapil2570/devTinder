f4185b631cf66b4efcd6e6342bd87e4147b30adb
Kapil Sahu <kapilsahu@proeffico.com>
2026-01-02 12:26:01 +0530

    [UPDATE] DB Security

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     await mongoose.connect(
-        "mongodb+srv://aithkapil_db_user:2zT04PLWMkb2NGls@namaste-node.bxvhmxv.mongodb.net/devTinder"
+        process.env.MONGODB_URI
     );
 }
 
