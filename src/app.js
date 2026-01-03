2d906fed22d974a8c54d34faa44e71820a6e8fe3
Kapil Sahu <kapilsahu@proeffico.com>
2026-01-03 12:12:49 +0530

    [UPDATE] Data Sanitization

--- src/app.js
+++ src/app.js
@@ -5,14 +5,40 @@ require('dotenv').config();
 
 const app = express();
 
-app.post("/signup", async (req,res) => {
-    const user = new User({
-        firstName: 'Atif',
-        lastName: 'Aslam',
-        emailId: 'atif@aslam.com',
-        password: 'atif@050'
-    })
+app.use(express.json());
 
+app.get("/user", async (req,res) => {
+
+    const userEmail = req.body.emailId;
+
+    try  {
+        const users = await User.find({ emailId : userEmail});
+        if(users.length === 0) {
+            res.status(404).send("User not found");
+        } else {
+            res.send(users);
+        }
+    } catch (err) {
+        res.status(400).send("Something went wrong");
+    }
+})
+
+app.get("/feed", async (req,res) => {
+    try {
+        const users = await User.find({});
+        if(users.length === 0) {
+            res.status(404).send("Feed is empty");
+        } else {
+            res.send(users);
+        }
+    } catch (err) {
+        res.status(400).send("Something went wrong");
+    }
+})
+
+app.post("/signup", async (req,res) => {
+
+    const user = new User(req.body);
     try {
         await user.save();
         res.send('User Created Successfully');
@@ -20,6 +46,32 @@ app.post("/signup", async (req,res) => {
         res.status(400).send("Eror saving the user: " + err.message);
     }
 })
+
+app.patch("/user/:userId", async (req,res) => {
+    const data = req.body;
+    const userId = req.params?.userId;
+    const ALLOWED_UPDATES = ["gender", "age", "photoUrl", "about", "skills"];
+    try {
+        const isUpdateAllowed = Object.keys(data).every(key => ALLOWED_UPDATES.includes(key));
+        if(!isUpdateAllowed)
+            throw new Error("Update not allowed")
+        const user = await User.findByIdAndUpdate(userId, data, { returnDocument: 'after', runValidators: true });
+        res.send("User updated successfully");
+    } catch (err) {
+        res.status(400).send("UPDATE FAILED:" + err.message);
+    }
+})
+
+app.delete("/user", async (req,res) => {
+    const userId = req.body.userId;
+    try {
+        const user = await User.findByIdAndDelete(userId);
+        res.send("User deleted successfully");
+    } catch (err) {
+        res.status(400).send("Eror saving the user: " + err.message);
+    }
+})
+
 
 connectDB()
     .then(() => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,73 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true,
+        minLength: 4,
+        maxLength: 20
     },
     lastName: {
-        type: String
+        type: String,
+        minLength: 2,
+        maxLength: 20
     },
     emailId: {
-        type: String
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        validate(val) {
+            if(!(validator.isEmail(val)))
+                throw new Error('Invalid Email Address: ' + val);
+        }
     },
     password: {
-        type: String
+        type: String,
+        required: true,
+        validate(val) {
+            if(!validator.isStrongPassword(val))
+                throw new Error("Weak Password: " + val);
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        min: 18
     },
     gender: {
-        type: String
+        type: String,
+        validate(value) {
+            if(!['male', 'female', 'others'].includes(value.toLowerCase())) {
+                throw new Error('Gender data is not valid');
+            }
+        },
+        lowercase: true
+    },
+    photoUrl: {
+        type: String,
+        default: "https://commons.wikimedia.org/wiki/File:Sample_User_Icon.png",
+        validate(val) {
+            if(!validator.isURL(val))
+                throw new Error("Invalid Photo URL");
+        }
+    },
+    about: {
+        type: String,
+        default: "This is default about statement"
+    },
+    skills: {
+        type: [String],
+        validate(val) {
+            if(val.length > 10)
+                throw new Error("Maximum 10 skills are allowed")
+        }
     }
-})
+},
+{
+    timestamps: true
+}
+)
 
 module.exports = mongoose.model("User", userSchema);
